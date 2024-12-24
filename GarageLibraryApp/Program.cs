a18e99a297ad01ffbb56e735c45ca54b8cd3b907
308 <308@kp15.lan>
2024-12-24 10:17:39 +0300

    Initial commit: Added Garage and Car classes

---
+++ GarageLibraryApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GarageLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем объект гаража
+        Garage garage = new Garage
+        {
+            Name = "Central Garage",
+            Address = "Стараватутинский дом 127"
+        };
+
+        // Добавляем автомобили
+        garage.Cars.Add(new Car { Owner = "Bmw", Number = "A127BC" });
+        garage.Cars.Add(new Car { Owner = "Audi", Number = "B436DE" });
+        garage.Cars.Add(new Car { Owner = "Xiaomi", Number = "C739FG" });
+
+        // Выводим информацию о всех автомобилях
+        garage.ShowAll();
+    }
+}
